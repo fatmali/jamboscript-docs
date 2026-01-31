d3d53d4de2a71ad1fc10a8fd10de6d39760dcd01
Fatma Ali <fali74@gatech.edu>
2026-02-01 00:11:03 +0300

    Update baseUrl to /jamboscript/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://fatmali.github.io',
-  baseUrl: '/',
+  baseUrl: '/jamboscript/',
 
   organizationName: 'fatmali',
   projectName: 'jamboscript-docs',
