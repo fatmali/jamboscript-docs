3dc88c409518ff1ddd5f3cf72b8a2a56f699bf32
Fatma Ali <fali74@gatech.edu>
2026-02-08 23:36:31 +0300

    Updates

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/jamboscript-docs",
+  basePath: "",
   images: {
     unoptimized: true,
   },
